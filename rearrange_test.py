9b57b6f375f6b4fdfa9183c2a3f00aec32508a40
vakils <shrey.vakil@gmail.com>
2020-06-01 13:37:03 -0700

    windows import issue

--- rearrange_test.py
+++ rearrange_test.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 import  unittest
-from .rearrange  import rearrange_name
+import rearrange
 
 class TestRearrange(unittest.TestCase):
     def test_basic(self):
         testcase = "Lovelasce Ada"
         expected = "Ada Lovelasce"
-		self.assertEqual(rearrange_name(testcase), expected)
+        self.assertEqual(rearrange.rearrange_name(testcase), expected)
 
 
     def test_emty(self):
-		testcase = ""
-		expected = ""
-		self.assertEqual(rearrange_name(testcase), expected)
+        testcase = ""
+        expected = ""
+        self.assertEqual(rearrange.rearrange_name(testcase), expected)
 
 
     def test_double_name(self):
         testcase = "Hopper, Grace M."
         expected = "Grace M. Hopper"
-        self.assertEqual(rearrange_name(testcase), expected)
-
+        self.assertEqual(rearrange.rearrange_name(testcase), expected)
 
 
 unittest.main()
